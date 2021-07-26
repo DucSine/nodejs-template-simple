136eb554ba8e2ec463a9736cc86b43dea9cf592e
DucSine <duchoangvo97@gmail.com>
2021-07-26 08:55:29 +0700

    feature:soket.io

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config()
-const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const app = express()
+const { app, server } = require('./helpers/handleSocketIo.helper')
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello'))
 
-app.listen(port, () => console.log(`run: http://localhost:${port}`))
+server.listen(port, () => console.log(`run: http://localhost:${port}`))
